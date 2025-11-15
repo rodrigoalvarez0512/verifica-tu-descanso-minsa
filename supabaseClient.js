716f6b46362533148ea829174e56c63174a25213
rodrigoalvarez0512 <rodrigoalvarez0512@gmail.com>
2025-11-15 15:23:00 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,7 @@
 const { createClient } = supabase;
 
 const SUPABASE_URL = 'https://kptkfgohekpvebusongi.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwdGtmZ29oZWtwdmVidXNvbmdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3MjYwMzksImV4cCI6MjA3NTMwMjAzOX0.nB5BQCDNpqqwwYEEvc1aLV12V1H1KNnSC_sjtTN8KYk';
+const SUPABASE_KEY = 'sb_publishable_aCqDjWZQrykfCXQWOqm-MA_0ISOoxVy';
 
 const clienteSupabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
